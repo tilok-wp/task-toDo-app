b4922201624bb025378fd08cf6dd6cdc47cc8eac
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-19 07:50:07 +0600

    Update home page ontent

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [lists, setLists] = useState([])
+
+    const addListSubmit = (e) => {
+        e.preventDefault()
+        const taskName = e.target.taskName.value
+        const taskDesc = e.target.taskDesc.value
+        let liteItems = [lists]
+        const addItem = { taskName, taskDesc }
+        const newLists = liteItems.push(addItem)
+        setLists(newLists)
+    }
+
+
+
+
     return (
         <div>
-            <form >
+            <form onSubmit={() => addListSubmit()}>
                 <input type="text" name='taskName' placeholder='Task name' required />
                 <input type="text" name='taskDesc' placeholder='Task Description' required />
                 <input type="submit" value="Add" />
             </form>
+
+            <div>
+                <h3>All list {lists.length}</h3>
+                <ul>
+                    {lists.map((item, i) => <li key={i}>Taks: {item.taskName}  Description: {item.taskDesc} <button>Complete</button> <button>X</button></li>)}
+                    <li></li>
+                </ul>
+            </div>
         </div>
     );
 };
